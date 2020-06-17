6832bd038d67de6976057fdb17d19da0446abc07
Anna Langner <annalangner06@gmail.com>
2020-06-17 22:11:37 +0200

    Modify script to accept the additional parameter during the run

--- script/hello_name.py
+++ script/hello_name.py
@@ -1,2 +1,5 @@
-name = input('Whats Your name?')
-print('Hello', name)
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("name", help="name the string you are use here")
+args = parser.parse_args()
+print('Hello', args.name)
