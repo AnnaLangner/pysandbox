3a8bed1e5eee748a1b24105e77a570f161e45426
Anna Langner <annalangner06@gmail.com>
2020-06-17 22:37:30 +0200

    Modify script to accept the additional parameter during the run

--- script/min.py
+++ script/min.py
@@ -1,6 +1,12 @@
-x = input('Entry first number:')
-y = input('Entry second number:')
-if float(x) > float(y) : 
-  print(y)
-if float(x) < float(y) :
-  print(x)
+#This script should take 2 parameters (numbers) and print lower number on the console.
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("num1", help="num is the int you use here")
+parser.add_argument("num2", help="num is the int you use here")
+args = parser.parse_args()
+if float(args.num1) > float(args.num2) : 
+  print("Lower number:", args.num2)
+elif float(args.num1) < float(args.num2) :
+  print("Lower number:", args.num1)
+else :
+  print("Numbers are equal")
