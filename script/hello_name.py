2f8e34f3191301455052e8344dcdf64c149d529e
Anna Langner <annalangner06@gmail.com>
2020-06-18 11:36:03 +0200

    Improved code syntax

--- script/hello_name.py
+++ script/hello_name.py
@@ -4,9 +4,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", help="name the string you are use here")
 args = parser.parse_args()
-
-if args.name :
-  print('Hello', args.name)
-else :
-  args.name = input('Whats Your name?')
-  print('Hello', args.name)
+if args.name:
+ print('Hello', args.name)
+else:
+ args.name = input('Whats Your name?')
+ print('Hello', args.name)
+  
