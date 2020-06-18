b8e5c1f49765a967e56bbc41e92bca4d42856311
Anna Langner <annalangner06@gmail.com>
2020-06-18 14:48:37 +0200

    Improved indentation

--- script/hello_name.py
+++ script/hello_name.py
@@ -7,6 +7,6 @@ args = parser.parse_args()
 if args.name:
   print('Hello', args.name)
 else:
- args.name = input('Whats Your name?')
+  args.name = input('Whats Your name?')
   print('Hello', args.name)
   
