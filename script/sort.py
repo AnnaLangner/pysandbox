54fcc3dfb666804ba3637fd2dc885bfdf0575176
Anna Langner <annalangner06@gmail.com>
2020-06-18 21:51:38 +0200

    Add loop

--- script/sort.py
+++ script/sort.py
@@ -4,4 +4,6 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('fruit', nargs='*')
 args = parser.parse_args()
-print("Sorted list:", sorted(args.fruit))
+sortList = sorted(args.fruit)
+for i in sortList:
+  print(i)
