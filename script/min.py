fbd08d6c139e1cae0a1925ec63ec896de6600c43
Anna Langner <annalangner06@gmail.com>
2020-06-18 09:38:30 +0200

    Improved code syntax

--- script/min.py
+++ script/min.py
@@ -1,12 +1,15 @@
 #This script should take 2 parameters (numbers) and print lower number on the console.
 import argparse
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("num1", help="num is the int you use here")
 parser.add_argument("num2", help="num is the int you use here")
 args = parser.parse_args()
-if float(args.num1) > float(args.num2) : 
+if float(args.num1) > float(args.num2): 
   print("Lower number:", args.num2)
-elif float(args.num1) < float(args.num2) :
+elif float(args.num1) < float(args.num2):
   print("Lower number:", args.num1)
-else :
+else:
   print("Numbers are equal")
+  
