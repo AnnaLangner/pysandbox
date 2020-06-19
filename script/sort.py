119178a5c850dc4672ef4eac72081c05fbe6ef49
Anna Langner <annalangner06@gmail.com>
2020-06-19 12:06:12 +0200

    Use join() Method

--- script/sort.py
+++ script/sort.py
@@ -5,4 +5,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('fruit', nargs='*')
 args = parser.parse_args()
 sort_list = sorted(args.fruit)
-print(*sort_list, sep=', ')
+#the first way
+#print(*sort_list, sep=', ')
+#the second way
+fruits = ", ".join(sort_list)
+print(fruits)
