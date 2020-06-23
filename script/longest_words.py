851e1af0c0a1d829393e14521d08c74e18ebbe35
Anna Langner <annalangner06@gmail.com>
2020-06-23 14:57:15 +0200

    Added regex

--- script/longest_words.py
+++ script/longest_words.py
@@ -1,5 +1,5 @@
 import argparse
-import string
+import re
 
 
 parser = argparse.ArgumentParser()
@@ -7,12 +7,19 @@ parser.add_argument('file', help='Add path to the file')
 args = parser.parse_args()
 path = args.file
 file = open(path)
-words = file.read().split()
-words_normalize = []
-for word in words:
-  words_lower = word.lower()
-  words_punctuation = words_lower.translate(str.maketrans('', '', string.punctuation))
-  words_whitespaces = words_punctuation.strip()  
-  words_normalize.append(words_whitespaces)
-  words_normalize.sort(key=len, reverse=True)
-print(words_normalize[0], words_normalize[1], words_normalize[2])
+#without regex & with replace()
+# words = file.read().split()
+# words_normalize = []
+# for word in words:
+#   words_lower = word.lower()
+#   words_punctuation = words_lower.replace(",", " ").replace(".", " ").replace("-", "")
+#   words_whitespaces = words_punctuation.strip()  
+#   words_normalize.append(words_whitespaces)
+# words_normalize.sort(key=len, reverse=True)
+# print(words_normalize[0], words_normalize[1], words_normalize[2])
+#
+#with regex
+words = file.read()
+words = re.sub("\W", " ", words).lower().split()
+words.sort(key=len, reverse=True)
+print(words[0], words[1], words[2])
