6cc30ab44dcd14e1165fda81c13d3a813d5a6b74
Anna Langner <annalangner06@gmail.com>
2020-06-27 17:16:23 +0200

    Added function "add_characters()"

--- script/fetch_content.py
+++ script/fetch_content.py
@@ -10,6 +10,12 @@ def arguments():
   parser.add_argument('--text', help='add text')
   args = parser.parse_args()
   return args
+def add_characters():
+  length_text = len(text)
+  start = r.text.index(text)
+  new_text.append(r.text[start:10+start])
+  new_text.append(r.text[start:start+(length_text+10)])    
+  print(''.join(new_text))
 args = arguments()
 url = args.url
 text = args.text
@@ -17,17 +23,13 @@ new_text = []
 r = requests.get(url)
 if args:
   if text in r.text:
-    length_text = len(text)
-    start = r.text.index(text)
-    new_text.append(r.text[start:10+start])
-    new_text.append(r.text[start:start+(length_text+10)])    
-    print(''.join(new_text))
+    add_characters()
   else:
     print("Text '" + text + "' not found")
 else:
   url = input('Enter the URL:')
   text = input('Enter the search phrase: ')
   if text in r.text:
-    print(text)
+    add_characters()
   else:
     print("Text '" + text + "' not found")
