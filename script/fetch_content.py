9dfb61bad3c084ab787b86b232b5a910a4cfc402
Anna Langner <annalangner06@gmail.com>
2020-06-30 20:32:24 +0200

    Fourth attempt to clean the code

--- script/fetch_content.py
+++ script/fetch_content.py
@@ -16,35 +16,36 @@ def fetch_arguments():
 
 def print_10_characters_before_and_after_text(search_phrase, text):
   """
-  Print text with  additional characters.
+  Print text with additional characters.
 
   This function retrieves the text and returns it with an additional ten characters before and after the text.
 
   parameters:
-  text: text that someone enters
-  r.text: all text on the website
-  start: index of the first letter of the entered text
-  end: index of the last letter of the entered text
+  search_phrase: text that someone enters.
+  r.text: all text on the website.
+  start: index of the first letter of the entered text.
+  end: index of the last letter of the entered text.
   new_text: a string with additional characters added before start and after end.
+  list_character: list containing elements consisting of 10 characters.
 
   Return:
-  string: text with 10 additional characters before and after.
+  list character.
   
-  """
-   
-  if search_phrase in r.text:
-    for m in re.finditer(search_phrase, r.text):
-      start = m.start()
-      end = m.end()
-      new_text = r.text[start-10:end+10]
-      return new_text
+  """ 
+  
+
+  first_occurrence = r.text.find(search_phrase)
+  if first_occurrence >= 0:
+    found_text = re.search(search_phrase, r.text)
+    start = found_text.start()
+    end = found_text.end()
+    new_text = r.text[start-10:end+10]
+    list_character = new_text.split(search_phrase, 2)
+    return list_character
   else:
     print("Text '" + search_phrase + "' not found") 
 
 
-try:
-  (url, search_phrase) = fetch_arguments()
-  r = requests.get(url) 
-  print(print_10_characters_before_and_after_text(search_phrase, r.text).split(search_phrase, 2))
-except:
-  print("Error: This code works when enter the url and text.")  
+(url, search_phrase) = fetch_arguments()
+r = requests.get(url) 
+print(print_10_characters_before_and_after_text(search_phrase, r.text))
