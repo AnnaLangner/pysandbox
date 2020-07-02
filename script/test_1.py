067e1b09e269af09f462d6061ac28f9aeba7554b
Anna Langner <annalangner06@gmail.com>
2020-07-02 09:40:58 +0200

    Path change

--- script/test_1.py
+++ script/test_1.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Chrome(executable_path="D:\Programowanie\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="bin\chromedriver_win32\chromedriver.exe")
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_class_name("q")
