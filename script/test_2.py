2b4562eae37ca740ddde2a1107f36c150af53dfe
Anna Langner <annalangner06@gmail.com>
2020-07-03 15:01:40 +0200

    The second attempt to write an automatic test

--- script/test_2.py
+++ script/test_2.py
@@ -3,25 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
   
 
-def test_search_in_wikipedia_org():
+def main():
   driver = webdriver.Chrome(executable_path="bin/chromedriver")
   driver.get("https://en.wikipedia.org/wiki/Main_Page")
   search_input = driver.find_element_by_id("searchInput")
+  search_input.clear()
   search_input.send_keys("Unit Testing")
-  search_input.send_keys(Keys.ENTER)
-  assertIn("Unit Testing", driver.title)
-  searched_link = driver.find_element_by_link_text("/wiki/Halting_problem")
+  search_input.submit()
+  if "Unit Testing" in driver.current_url:
+    print("We are on a good site")
+  searched_link = driver.find_element_by_link_text("halting problem")
+  searched_link.click()
   phrase_1 = "Turing machine"
   phrase_2 = "no needed"
-  body_text = driver.find_element_by_id("bodyContent").text
-  assertTrue(phrase_1 in body_text)
-  assertFalse(phrase_2 in body_text)
-  # second way to search for a phrase
-  # src = driver.page_source
-  # found_phrase_1 = re.search(r"Turing machine", src)
-  # assert "No result found." not in src
+  src = driver.page_source
+  found_phrase_1 = re.search(phrase_1, src)  
+  assert "No result found." not in src
+  found_phrase_2 = re.search(phrase_2, src)
+  assert "No result found." in src
   driver.close()
 
 
-test_search_in_wikipedia_org()
+main()
   
