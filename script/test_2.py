92b29b2619de8f75c775e97d041bc2d01c0c4ad8
Anna Langner <annalangner06@gmail.com>
2020-07-06 11:54:33 +0200

    The last attempt to write an automatic test

--- script/test_2.py
+++ script/test_2.py
@@ -7,13 +7,11 @@ def main():
   driver.get("https://en.wikipedia.org/wiki/Main_Page")
   search_input = driver.find_element_by_id("searchInput")
   search_input.clear()
-  search_input.send_keys("Unit Testing")
+  search_input.send_keys("Unit testing")
   search_input.submit()
-  if "Unit Testing" in driver.current_url:
-    print("We are on a good site")
+  assert "Unit testing" in driver.title
   searched_link = driver.find_element_by_link_text("halting problem")
   searched_link.click()
-  content_page = driver.find_element_by_id('content')
   src = driver.page_source
   driver.find_elements_by_xpath("//*[contains(text(), 'Turing machine')]")  
   assert "No result found." not in src
