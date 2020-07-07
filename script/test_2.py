4932c77bd7da339ae433beefab40c0a661bf827a
Anna Langner <annalangner06@gmail.com>
2020-07-07 13:02:15 +0200

    Changed sleep() to WebDriverWait

--- script/test_2.py
+++ script/test_2.py
@@ -1,6 +1,9 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
   
 
 def main():
@@ -11,8 +14,10 @@ def main():
   search_input.send_keys("Unit testing")
   search_input.submit()
   assert "Unit testing" in driver.title
-  searched_link = driver.find_element_by_link_text("halting problem")
-  time.sleep(5)
+  #time.sleep(5)
+  searched_link = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.LINK_TEXT, "halting problem")))
+  searched_link = driver.find_element_by_link_text("halting problem")  
   searched_link.click()  
   phrase_1 = driver.find_elements_by_xpath("//*[text()='Turing machine']")
   print(phrase_1)
