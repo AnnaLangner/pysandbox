7bd340085239041cb28a36a3df33df34bc1a6436
Anna Langner <annalangner06@gmail.com>
2020-07-13 14:24:26 +0200

    code cleaning

--- script/test_todo.py
+++ script/test_todo.py
@@ -13,33 +13,43 @@ def add_task(driver, title):
   input_todo.send_keys(Keys.RETURN)
 
 
+def active_task(driver, id):
+  elem = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[%s]"%str(id))
+  try:
+    class_elem = elem.get_attribute("class")
+    if(class_elem == "completed"):
+      return True
+  except:
+    NoSuchAttributeException
+
+
+def comparison_of_adjacent_tasks(driver, id):
+  elem_1 = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[%s]"%str(id))
+  elem_2 = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[%s]"%str(id+1))
+  if(elem_1.text != elem_2.text):
+    pass
+  else:
+    print("You already have this task")
+
+
+def task_completed(driver, id):
+  elem = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[%s]"%str(id))
+  elem_toggle = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[%s]/div/input"%str(id))
+  elem_toggle.click()
+  assert elem.get_attribute("class") == "completed"
+
+
+
 def main():
   driver = webdriver.Chrome(executable_path="bin/chromedriver")
   driver.get("http://todomvc.com/examples/react/#/")
   WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//html/body/section/div/header/input")))
   add_task(driver, "task1")
-  elem_1 = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[1]")
-  try:
-    class_1 = elem_1.get_attribute("class")
-    if(class_1 == "completed"):
-      return True
-  except:
-    NoSuchAttributeException 
+  active_task(driver, 1)
   add_task(driver, "task2")
-  elem_2 = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[2]")
-  if(elem_1.text != elem_2.text):
-    pass
-  else:
-    print("You already have this task")
-  try:
-    class_2 = elem_2.get_attribute("class")
-    if(class_2 == "completed"):
-      return True
-  except:
-    NoSuchAttributeException 
-  elem_2_toggle = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[2]/div/input")
-  elem_2_toggle.click()
-  assert elem_2.get_attribute("class") == "completed"    
+  comparison_of_adjacent_tasks(driver, 1)  
+  active_task(driver, 2) 
+  task_completed(driver, 2)
   driver.close()
 
 
