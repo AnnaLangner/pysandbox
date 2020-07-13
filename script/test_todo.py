9b10587894b266438e7bbbb3620dfaed27446b5d
Anna Langner <annalangner06@gmail.com>
2020-07-13 13:11:49 +0200

    code cleaninig

--- script/test_todo.py
+++ script/test_todo.py
@@ -6,14 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchAttributeException
 
 
+def add_task(driver, title):
+  input_todo = driver.find_element_by_xpath("//html/body/section/div/header/input")
+  input_todo.clear()
+  input_todo.send_keys(title)
+  input_todo.send_keys(Keys.RETURN)
+
+
 def main():
   driver = webdriver.Chrome(executable_path="bin/chromedriver")
   driver.get("http://todomvc.com/examples/react/#/")
   WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//html/body/section/div/header/input")))
-  input_todo = driver.find_element_by_xpath("//html/body/section/div/header/input")
-  input_todo.clear()
-  input_todo.send_keys("task1")
-  input_todo.send_keys(Keys.RETURN)
+  add_task(driver, "task1")
   elem_1 = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[1]")
   try:
     class_1 = elem_1.get_attribute("class")
@@ -21,10 +25,7 @@ def main():
       return True
   except:
     NoSuchAttributeException 
-  input_todo = driver.find_element_by_xpath("//html/body/section/div/header/input")
-  input_todo.clear()
-  input_todo.send_keys("task2")
-  input_todo.send_keys(Keys.RETURN)
+  add_task(driver, "task2")
   elem_2 = driver.find_element_by_xpath("/html/body/section/div/section/ul/li[2]")
   if(elem_1.text != elem_2.text):
     pass
